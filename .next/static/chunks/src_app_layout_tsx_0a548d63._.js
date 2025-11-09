10740b416fb172acf3cf8ee3f3476de9bd950165
melihzafer <melihzafer@users.noreply.github.com>
2025-11-09 16:30:28 +0200

    ASDasd

--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__258b9112._.css",
-  "static/chunks/node_modules_562fcd17._.js",
-  "static/chunks/src_60f0eab5._.js"
-],
-    source: "dynamic"
-});
--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -90,6 +90,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../src/app/restaurant/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/restaurant">> = Specific
+  const handler = {} as typeof import("../../src/app/restaurant/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/samuil-hub/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/samuil-hub">> = Specific
